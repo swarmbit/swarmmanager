f02792c065d40f0b0caf33e820c7446a30d04331
Bruno Vale <bruno_vale@outlook.com>
2017-10-22 22:03:44 +0100

    Tests fixed

--- swarmmanagercontroller/src/test/java/com/swarmmanager/SwarmManagerControllerApplicationTests.java
+++ swarmmanagercontroller/src/test/java/com/swarmmanager/SwarmManagerControllerApplicationTests.java
@@ -1,5 +1,6 @@
 package com.swarmmanager;
 
+import com.swarmmanager.integration.IntegrationTest;
 import org.junit.Test;
 import org.junit.experimental.categories.Category;
 import org.junit.runner.RunWith;
@@ -8,7 +9,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class SwarmManagerControllerApplicationTests {
+public class SwarmManagerControllerApplicationTests extends IntegrationTest {
 
 	@Test
 	public void contextLoads() {
--- swarmmanagercontroller/src/test/java/com/swarmmanager/docker/config/DockerConfigTest.java
+++ swarmmanagercontroller/src/test/java/com/swarmmanager/docker/config/DockerConfigTest.java
@@ -2,21 +2,13 @@ package com.swarmmanager.docker.config;
 
 import com.swarmmanager.integration.IntegrationTest;
 import org.junit.Test;
-import org.junit.experimental.categories.Category;
-import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.List;
 
 import static org.junit.Assert.assertTrue;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest
-@Category(IntegrationTest.class)
-public class DockerConfigTest {
+public class DockerConfigTest extends IntegrationTest {
 
     @Autowired
     private DockerConfig dockerConfig;
